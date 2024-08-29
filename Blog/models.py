121d1b4a3d7f14c402296f015f66afc669474934
Louis Remy <louis.remy@acelys.fr>
2024-08-29 15:24:39 +0200

    Ajout gestion user et session user

--- Blog/models.py
+++ Blog/models.py
@@ -1,12 +1,68 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User, AbstractUser, BaseUserManager
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a user with the given email, logo and password.
+        """
+        user = self.model(
+            email = self.normalize_email(email),
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+        
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email, logo and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            **extra_fields
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    objects = UserManager()
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Utilisateur'
+
+    def __str__(self):
+        return f'{self.username}'
+    
+
 class Session(models.Model):
     session_name = models.TextField("name", default = "")
     session_id = models.AutoField(primary_key=True)
 
+    def __str__(self):
+        return f"{self.session_name} ({self.session_id})"
 
+class SessionUser(models.Model):
+    session = models.ForeignKey(Session, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} ({self.session})"
+    
 class Message(models.Model):
 
     pub_date = models.DateTimeField("Date publication")
@@ -19,8 +75,6 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.writer} ({self.pub_date}) : {self.text}"
-
-    
 
 class History(models.Model):
 
