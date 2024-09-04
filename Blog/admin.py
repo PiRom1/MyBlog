90586e1b20c6e612cead94fcd142ceb0489d88e5
Louis Remy <louis.remy@acelys.fr>
2024-09-04 11:50:55 +0200

    ticket admin

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,6 +1,9 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
 from .models import *
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.db.models.functions import Lower, Reverse
 class UserAdmin(DefaultUserAdmin):
     model = User
     list_display = ['username', 'is_superuser']
@@ -25,3 +28,10 @@ class SessionUserAdmin(admin.ModelAdmin):
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('image',)
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'status',)
+
+    def get_ordering(self, request):
+        return ['-status']
