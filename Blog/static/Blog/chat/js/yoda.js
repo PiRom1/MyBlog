eb62e8804c0a94a182aa84317b0ce7985f6c0219
Louis Remy <louis.remy@acelys.fr>
2024-10-03 18:01:16 +0200

    rm console log

--- Blog/static/Blog/chat/js/yoda.js
+++ Blog/static/Blog/chat/js/yoda.js
@@ -2,10 +2,8 @@ function playYodaSound() {
     
     var audioPlayer = document.getElementById('audio-player');
     var audioSource = document.getElementById('audio-source');
-    console.log("Yoda sounds : ", document.getElementById("yoda_sounds"));
     var yoda_sounds = document.getElementById("yoda_sounds").innerHTML;
     var static_url = document.getElementById("yoda_sounds").getAttribute("url");
-    console.log("Static URL : ", static_url);
 
     yoda_sounds = yoda_sounds.replace(/'/g, '"');
     yoda_sounds = JSON.parse(yoda_sounds);
@@ -26,7 +24,6 @@ function playYodaSound() {
 // Ajout d'un écouteur d'événement au clic sur l'image
 document.addEventListener('DOMContentLoaded', function () {
     var yoda = document.getElementById('yoda');
-    console.log("Yoda : ", yoda);
     yoda.addEventListener('click', playYodaSound);
 });
 
