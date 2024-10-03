25f211c122c33b79f1ff5040103bcf37d3a232c1
PiRom1 <romain@pfsb.org>
2024-10-03 11:28:20 +0200

    statics

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = './Blog/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "Blog/static")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
