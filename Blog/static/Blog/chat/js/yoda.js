6ae95d742c04910c2692a2cc89fd6fb677b70da3
Louis Remy <louis.remy@acelys.fr>
2024-10-03 18:00:01 +0200

    fix yoda + chargement messages

--- Blog/static/Blog/chat/js/yoda.js
+++ Blog/static/Blog/chat/js/yoda.js
@@ -1,16 +1,11 @@
-
-
-console.log("TEST !");
-
-
-
-
-
 function playYodaSound() {
     
     var audioPlayer = document.getElementById('audio-player');
     var audioSource = document.getElementById('audio-source');
+    console.log("Yoda sounds : ", document.getElementById("yoda_sounds"));
     var yoda_sounds = document.getElementById("yoda_sounds").innerHTML;
+    var static_url = document.getElementById("yoda_sounds").getAttribute("url");
+    console.log("Static URL : ", static_url);
 
     yoda_sounds = yoda_sounds.replace(/'/g, '"');
     yoda_sounds = JSON.parse(yoda_sounds);
@@ -29,5 +24,9 @@ function playYodaSound() {
 };
 
 // Ajout d'un écouteur d'événement au clic sur l'image
-document.getElementById('yoda').addEventListener('click', playYodaSound);
+document.addEventListener('DOMContentLoaded', function () {
+    var yoda = document.getElementById('yoda');
+    console.log("Yoda : ", yoda);
+    yoda.addEventListener('click', playYodaSound);
+});
 
