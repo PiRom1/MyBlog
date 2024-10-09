9b34b60860eda3b1ea0401ae4387d882b07c8d3a
PiRom1 <romain@pfsb.org>
2024-10-09 16:44:44 +0200

    Baisse délais action graphe user à 150ms

--- Blog/static/Blog/user/js/slider.js
+++ Blog/static/Blog/user/js/slider.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
                
             })
             .catch(error => console.error('Error:', error));
-    }, 300);
+    }, 150);
     
 
 };
