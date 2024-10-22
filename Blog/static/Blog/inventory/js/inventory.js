76eb159dce9b560bff8eca71e5571079ca11e894
Louis Remy <louis.remy@acelys.fr>
2024-10-22 15:32:54 +0200

    visuel inventory

--- Blog/static/Blog/inventory/js/inventory.js
+++ Blog/static/Blog/inventory/js/inventory.js
@@ -1,3 +1,5 @@
+import autocomplete from './autocomplete.js';
+
 // JavaScript pour gérer les événements de clic
 document.addEventListener('DOMContentLoaded', function () {
     const items = document.querySelectorAll('.inventory-item');
@@ -13,15 +15,55 @@ document.addEventListener('DOMContentLoaded', function () {
     const tradeOption = document.getElementById('trade-option');
     const equipOption = document.getElementById('equip-option');
     const openOption = document.getElementById('open-option');
+    
+    // Récupérer les noms des items
+    const searchList = ['equipped', 'unequipped', 'locked', 'box', 'skin'];
+
+    function addHeartCircle(item) {
+        // Créer un élément div pour le cercle avec le coeur
+        const heartCircle = document.createElement('div');
+        heartCircle.classList.add('heart-circle');
+
+        // Ajouter l'icône de coeur de FontAwesome à l'intérieur du cercle
+        heartCircle.innerHTML = '<i class="fas fa-heart"></i>';
+
+        // Ajouter le cercle avec l'icône de coeur à l'item
+        item.appendChild(heartCircle);
+    }
 
     // Mettre à jour l'événement de clic sur les items pour afficher les options du menu contextuel
     items.forEach(item => {
+
+        const itemName = item.getAttribute('data-name');
+        const itemType = item.getAttribute('data-type');
+        const itemStatus = item.getAttribute('data-status');
+        const itemSkinType = item.getAttribute('data-skin-type');
+
+        if (itemName && !searchList.includes(itemName)) {searchList.push(itemName);}
+        if (itemSkinType && !searchList.includes(itemSkinType)) {searchList.push(itemSkinType);}
+        
+        const pattern = item.getAttribute('data-pattern');
+        // Vérifier si le pattern commence par un '#' (hexadécimal)
+        if (pattern && pattern.startsWith('#')) {
+            // Créer un élément div pour le cercle
+            const colorCircle = document.createElement('div');
+            colorCircle.classList.add('color-circle');
+            
+            // Appliquer la couleur de fond correspondant à l'hexadécimal du pattern
+            colorCircle.style.backgroundColor = pattern;
+            
+            // Ajouter le cercle à l'item
+            item.appendChild(colorCircle);
+        }
+
+        // Vérifier si l'item est équipé
+        if (itemStatus === 'equipped') {
+            addHeartCircle(item)
+        }
+
         item.addEventListener('click', function (e) {
             e.preventDefault();
             selectedItem = item;
-    
-            const itemType = selectedItem.getAttribute('data-type');
-            const itemStatus = selectedItem.getAttribute('data-status');
             
             // Toujours afficher l'option "Infos"
             infoOption.style.display = 'block';
@@ -161,7 +203,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 if (data.success) {
                     // Mettre à jour le statut dans l'attribut de l'élément
                     selectedItem.setAttribute('data-status', newStatus);
-                    alert(`${selectedItem.getAttribute('data-name')} a été ${newStatus === 'equipped' ? 'équipé' : 'déséquipé'}.`);
+                    // Ajouter ou supprimer le cercle avec le coeur
+                    if (newStatus === 'equipped') {
+                        addHeartCircle(selectedItem);
+                    } else {
+                        const heartCircle = selectedItem.querySelector('.heart-circle');
+                        if (heartCircle) {
+                            heartCircle.remove();
+                        }
+                    }
+                    // alert(`${selectedItem.getAttribute('data-name')} a été ${newStatus === 'equipped' ? 'équipé' : 'déséquipé'}.`);
                 } else {
                     alert('Erreur lors de la mise à jour du statut : ' + data.message);
                 }
@@ -180,4 +231,38 @@ document.addEventListener('DOMContentLoaded', function () {
     equipOption.addEventListener('click', function () {
         toggleEquipStatus();
     });
+
+    // Récupérer l'élément de filtre
+    const filterButton = document.getElementById('filter-equipped');
+    // Ajouter un événement de clic pour activer ou désactiver le filtre
+    filterButton.addEventListener('click', function() {
+        // Basculer la classe "active" pour changer la couleur du bouton
+        filterButton.classList.toggle('active');
+        const showEquippedOnly = filterButton.classList.contains('active');
+        // Parcourir tous les items
+        items.forEach(item => {
+            const itemStatus = item.getAttribute('data-status');
+            // Si le filtre est activé et l'item n'est pas équipé, on le cache
+            if (showEquippedOnly && itemStatus !== 'equipped') {
+                item.style.display = 'none';
+            } else {
+                // Sinon, on affiche l'item
+                item.style.display = 'block';
+            }
+        });
+    });
+
+    // Récupérer l'élément de recherche
+    const searchInput = document.getElementById("search-bar");
+    // Ajouter un événement de saisie pour l'auto-complétion
+    autocomplete(searchInput, searchList);
+    // Récupérer l'icône de croix pour annuler le filtre
+    const clearFilterButton = document.getElementById('clear-filter');
+    // Réinitialiser la recherche et afficher tous les items lorsque la croix est cliquée
+    clearFilterButton.addEventListener("click", function() {
+        document.getElementById('search-bar').value = ""; // Vider la barre de recherche
+        items.forEach(item => item.style.display = 'block');
+        clearFilterButton.style.display = 'none'; // Masquer la croix
+    });
+
 });
