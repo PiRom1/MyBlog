c1001b47e14baf4cead5c3ff96a4f0d36d04d2de
PiRom1 <romain@pfsb.org>
2024-10-31 16:55:40 +0100

    Fix HDV + messages blancs au load more et auto refresh

--- Blog/static/Blog/hdv/js/hdv.js
+++ Blog/static/Blog/hdv/js/hdv.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Get attributes 
         const item_id = item.getAttribute("item_id");
-        const skin = item.getAttribute("skin");
+        const skin = item.getAttribute("skin-type");
         const seller = item.getAttribute("seller");
         const pattern = item.getAttribute("pattern");
         const price = item.getAttribute("price");
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         else {
             circle.style.display = "none";
-            if (skin === "Police") {
+            if (skin === "font") {
                 font_tab.push(pattern);
             }
         }
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if (pattern.startsWith('#')) {
                 popup_pattern.innerHTML = pattern + `<span class="color-circle" style="background-color: ${pattern};"></span>`;
             }
-            else if (skin === "Police") {
+            else if (skin === "font") {
                 popup_pattern.innerHTML = `<a style="font-family: '${pattern}'; color: #feefeb;">${pattern}</a>`;
             }
             else {
