3c6664bec7490535f5734851db617436d45c462b
PiRom1 <romain.pierlot@acelys.fr>
2024-11-07 16:25:25 +0100

    Viewed User background sur page profil

--- Blog/context_processors.py
+++ Blog/context_processors.py
@@ -3,10 +3,15 @@
 from .models import Skin, UserInventory, Background
 
 def background_context(request):
-    background_id = Skin.objects.get(type='background_image').id
-    bg = UserInventory.objects.filter(item_id__item_id=background_id).filter(equipped=True)
-    if bg:
-        bg = Background.objects.get(id=bg[0].item.pattern).image.url
-    else:
-        bg = None
+    
+    bg = None
+    if request.user.is_authenticated:
+
+        background_id = Skin.objects.get(type='background_image').id
+        bg = UserInventory.objects.filter(item_id__item_id=background_id).filter(equipped=True).filter(user=request.user)
+        if bg:
+            bg = Background.objects.get(id=bg[0].item.pattern).image.url
+        else:
+            bg = None
+
     return {'bg_url': bg}
--- Blog/static/Blog/js/bg.js
+++ Blog/static/Blog/js/bg.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     bg = document.getElementById('bg').getAttribute('bg');
-
-    if (bg) {
+    console.log('bg : ', bg);
+    if (bg != 'None') {
         document.body.style.backgroundImage = `url(${bg})`;
     }
 
