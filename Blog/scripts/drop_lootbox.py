8dadd97aa745938b1e26f9ecc7be1c1e7bcbe2b0
Louis Remy <louis.remy@acelys.fr>
2024-12-10 17:03:21 +0100

    drop lootbox script

---
+++ Blog/scripts/drop_lootbox.py
@@ -0,0 +1,13 @@
+from Blog.models import User, Item, UserInventory
+
+def run():
+    nb_drop = 2
+    nb_coins = 400
+    users = User.objects.all()
+    for user in users:
+        user.coins += nb_coins
+        user.save()
+        for _ in range(nb_drop):
+            UserInventory.objects.create(user=user, item=Item.objects.create(type='box'))
+
+    print(f'{nb_drop} lootboxes dropped for each user | {nb_coins} coins added to each user')
