832ade3db26ebee6f009274b3d243ff70d4d9a07
PiRom1 <romain@pfsb.org>
2025-01-15 17:18:38 +0100

    Script drop lootbox

--- scripts/drop_lootbox.py
+++ scripts/drop_lootbox.py
@@ -1,20 +1,22 @@
 from Blog.models import User, Item, UserInventory, Box
 import argparse
 
-def run(*args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('nb_drop', type=int, default=2)
-    parser.add_argument('nb_coins', type=int, default=400)
-    args = parser.parse_args(args)
+def run():
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--nb_drop', type=int, default=2)
+    # parser.add_argument('--nb_coins', type=int, default=400)
+    # args = parser.parse_args(args)
+    nb_drop = 2
+    nb_coins = 400
 
     users = User.objects.all()
     box_id = Box.objects.last().id
     for user in users:
-        user.coins += args.nb_coins
+        user.coins += nb_coins
         user.save()
-        for _ in range(args.nb_drop):
+        for _ in range(nb_drop):
             UserInventory.objects.create(user=user, item=Item.objects.create(type='box', item_id=box_id))
 
-    print(f'{args.nb_drop} lootboxes dropped for each user | {args.nb_coins} coins added to each user')
+    print(f'{nb_drop} lootboxes dropped for each user | {nb_coins} coins added to each user')
 
 
