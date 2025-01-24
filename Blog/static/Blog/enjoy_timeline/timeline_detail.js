af318147cd81f2f852a2abe0deb5c39b44993e03
PiRom1 <romain@pfsb.org>
2025-01-24 12:25:40 +0100

    Mise en forme datetime timeline + gestion heures inexistantes

--- Blog/static/Blog/enjoy_timeline/timeline_detail.js
+++ Blog/static/Blog/enjoy_timeline/timeline_detail.js
@@ -33,4 +33,39 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
 
+
+    // Manage non existent hour minutes
+    const url = window.location.href;
+    const splitted_url = url.split('/');
+    const minute = splitted_url[splitted_url.length - 2];
+    const hour = splitted_url[splitted_url.length - 3];
+
+    console.log('hour : ', hour, "mionute : ", minute);
+
+    if (minute < 0 || minute > 59 || hour < 0 || hour > 23) {
+        console.log("nope");
+        document.body.innerHTML = '';
+        const date = new Date();
+
+        const h1 = document.createElement("h1");
+        h1.innerHTML = `${hour}h ${minute}mn`;
+        document.body.appendChild(h1);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = `Petit malin va ... Cette heure n'existe pas et tu le sais très bien !`;
+        document.body.appendChild(h3);
+
+        const h4 = document.createElement("h4");
+        h4.innerHTML = `Alors arrête les clowneries et va vraiment remplir la timeline, il y a encore du pain sur la planche !`;
+        document.body.appendChild(h4);
+
+        const link = document.createElement("a");
+        link.href= `/enjoy_timeline/${date.getHours()}/${date.getMinutes()}/`;
+        link.text = `${date.getHours()}h${date.getMinutes()} n'est pas encore rempli par exemple !`;
+        document.body.appendChild(link);
+
+    }
+
+
+
 })
