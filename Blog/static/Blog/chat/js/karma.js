8c234ad8e549167d7487cf516c417118c1902981
PiRom1 <romain@pfsb.org>
2025-01-27 16:43:32 +0100

    Show karma by right click

--- Blog/static/Blog/chat/js/karma.js
+++ Blog/static/Blog/chat/js/karma.js
@@ -11,9 +11,24 @@ document.addEventListener('DOMContentLoaded', function () {
         karma = message.getAttribute('karma');
         console.log("karma : ", karma);
         console.log(message);
-        message.addEventListener('click', function() {
-            console.log('karma du message : ', karma);
+        message.addEventListener('contextmenu', function(e) {
+            e.preventDefault();
+            const karma = message.getAttribute('karma').split('>')[0];
+            console.log('karma du message : ', message.getAttribute('karma'));
+            const texte = document.createElement("span");
+            texte.textContent = karma;
+            texte.style.position = "absolute";
+            texte.style.left = `${e.pageX}px`;
+            texte.style.top = `${e.pageY}px`;
+            texte.style.background = "rgba(237, 213, 168, 0.75)";
+            texte.style.padding = "5px";
+            texte.style.border = "1px solid black";
+            texte.style.borderRadius = "5px";
+            document.body.appendChild(texte);
+            setTimeout(() => texte.remove(), 500);
           });
 
     });
+
+
 })
