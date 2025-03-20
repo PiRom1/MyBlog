a30905847e91bc989ae29dc0b3b4b1b2ec36911b
PiRom1 <romain@pfsb.org>
2025-03-20 11:31:37 +0100

    Remove scheduler

--- Blog/apps.py
+++ Blog/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
+# from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.triggers.cron import CronTrigger
 from django.apps import AppConfig
 
 
@@ -12,14 +12,14 @@ class BlogConfig(AppConfig):
     def ready(self):
         import Blog.signals  # Charger les signaux
 
-        scheduler = BackgroundScheduler()
-        scheduler.start()
-        from Blog.scheduled_tasks import drop_lootbox
+        # scheduler = BackgroundScheduler()
+        # scheduler.start()
+        # from Blog.scheduled_tasks import drop_lootbox
 
-        # box_trigger
-        box_trigger = CronTrigger(
-            year="*", month="*", day="*", hour="0", minute="0", second="0"
-        )
+        # # box_trigger
+        # box_trigger = CronTrigger(
+        #     year="*", month="*", day="*", hour="0", minute="0", second="0"
+        # )
         # scheduler.add_job(
         #     drop_lootbox, trigger=box_trigger, name="test task", kwargs = {'nb_coin' : 200, 'nb_box' : 1}
         # )
