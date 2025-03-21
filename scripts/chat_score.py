91eb78c628fff2dba1735b55eb20a908ed42f2b2
Louis Remy <louis.remy@acelys.fr>
2025-03-21 10:16:28 +0100

    [FIX] date chat score

--- scripts/chat_score.py
+++ scripts/chat_score.py
@@ -88,7 +88,7 @@ def get_text(date, user_data, model):
 def run():
     model = "llama-3.3-70b-specdec"
     # Date of yesterday
-    date = datetime.date.today() - datetime.timedelta(days=2)
+    date = datetime.date.today() - datetime.timedelta(days=1)
     user_data = analyse_chat(date=date, session_id=2, model=model)
 
     WINRATE_COINS = 50
