d6c7ac72b577f939536c2274804f6a380cd39296
Louis Remy <louis.remy@acelys.fr>
2025-04-14 11:39:12 +0200

    script to uptd stats

--- scripts/dw_updt_stats.py
+++ scripts/dw_updt_stats.py
@@ -1,5 +1,5 @@
 from Blog.utils.dw_battle_logic import *
-from Blog.models import DWUserDino, DWUserTeam, User
+from Blog.models import DWUserDino, DWUserTeam, User, DWDino
 from Blog.views.dinowars_views import calculate_total_stats
 import pandas as pd
 
@@ -11,15 +11,23 @@ def run():
     ]
     
     for dino_name, stat_name, stat_value in stats_to_update:
-        dino = DWUserDino.objects.get(dino__name=dino_name)
-        if stat_name in ['hp', 'atk', 'defense']:
-            lvl = dino.level
-            old_stat = getattr(dino, stat_name)
-            new_stat = old_stat + int(stat_value * pow(1.1, lvl - 1))
-            setattr(dino, stat_name, new_stat)
-            dino.save()
-        else:
-            old_stat = getattr(dino, stat_name)
-            new_stat = old_stat + stat_value
-            setattr(dino, stat_name, new_stat)
-            dino.save()
+        usr_dinos = DWUserDino.objects.filter(dino__name=dino_name)
+        for dino in usr_dinos :        
+            if stat_name in ['hp', 'atk', 'defense']:
+                lvl = dino.level
+                old_stat = getattr(dino, stat_name)
+                new_stat = old_stat + int(stat_value * pow(1.1, lvl - 1))
+                setattr(dino, stat_name, new_stat)
+                dino.save()
+            else:
+                old_stat = getattr(dino, stat_name)
+                new_stat = old_stat + stat_value
+                setattr(dino, stat_name, new_stat)
+                dino.save()
+
+        dino = DWDino.objects.get(name=dino_name)
+        stat_name = 'base_' + stat_name
+        old_stat = getattr(dino, stat_name)
+        new_stat = old_stat + stat_value
+        setattr(dino, stat_name, new_stat)
+        dino.save()
