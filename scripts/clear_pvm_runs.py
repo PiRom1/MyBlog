a99f35887c4efd3b1f34f7b03f69bc0d4b035269
Louis Remy <louis.remy@acelys.fr>
2025-09-18 10:44:56 +0200

    fix clear run script

--- scripts/clear_pvm_runs.py
+++ scripts/clear_pvm_runs.py
@@ -7,13 +7,16 @@ sys.path.append('/home/runner/work/MyBlog/MyBlog')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBlog.settings')
 django.setup()
 
-from Blog.models import DWPvmRun, DWPvmNewRun, DWPvmDino
+from Blog.models import DWPvmRun, DWPvmNewRun, DWPvmDino, DWUser
 
 def clear_old_pvm_runs():
     """Clear old PvM runs from the database."""
     DWPvmRun.objects.all().delete()
     DWPvmNewRun.objects.all().delete()
     DWPvmDino.objects.all().delete()
+    for dwuser in DWUser.objects.all():
+        dwuser.pvm_runs_td = 0
+        dwuser.save()
 
 def run():
     clear_old_pvm_runs()
